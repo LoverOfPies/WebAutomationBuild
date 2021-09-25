705afd08de5a2abe8a0f33d10916a5ab5446e4f8
Ilya Antipov <antipov@krista.ru>
2021-09-25 13:40:39 +0300

    Добавлена версионность и фильтры

--- src/db/models/provider/Product.py
+++ src/db/models/provider/Product.py
@@ -1,17 +1,17 @@
 from peewee import ForeignKeyField, DoubleField, DateField
 
 from app import db
+from src.db.Versioning import Versioning
 from src.db.models.material.Material import Material
 from src.db.models.provider.Provider import Provider
 
 
 # Товар
-class Product(db.Model):
+class Product(db.Model, Versioning):
     price = DoubleField(default=0, verbose_name='Цена')
     amount_for_one = DoubleField(default=0, verbose_name='Количество материала за 1 шт')
     provider = ForeignKeyField(Provider, backref='products', verbose_name='Поставщик')
     material = ForeignKeyField(Material, backref='products', verbose_name='Материал')
-    date_load = DateField(verbose_name='Дата выгрузки товаров')
 
     class Meta:
         db_table = "ab_product"
