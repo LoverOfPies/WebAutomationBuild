2e3498f0dfb9487e8744993bb77def71a0f490fa
Ilya Antipov <antipov@krista.ru>
2021-09-25 15:44:07 +0300

    Добавлен импорт

--- src/expimp/ImportUtils.py
+++ src/expimp/ImportUtils.py
@@ -1,15 +1,11 @@
 from openpyxl import load_workbook
 
 
-def import_single_row(file, model_class):
-    wb = load_workbook(file)
+def import_single_row(model, file_name):
+    wb = load_workbook(file_name)
     sheet = wb.get_sheet_by_name('Лист1')
     values = []
     for cell in sheet['A']:
         if cell.row == 1:
             continue
         values.append([{'name': str(cell.value)}])
-    data = dict([
-        ('model_class', model_class),
-        ('value', values),
-    ])
