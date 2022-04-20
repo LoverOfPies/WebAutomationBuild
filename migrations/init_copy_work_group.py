ad5415962d291afe919340b29f1074b8363386ed
LoverOfPies <ilya.408@yandex.ru>
2022-04-20 22:43:05 +0300

    Идемподентность для скрипта

--- migrations/init_copy_work_group.py
+++ migrations/init_copy_work_group.py
@@ -1,9 +1,9 @@
-from src.db.DataBaseUtils import insert_record, get_record
+from src.db.DataBaseUtils import get_record, get_or_insert
 from src.db.models.info.ActionInfo import ActionInfo
 from src.db.models.info.TableInfo import TableInfo
 
 
 def run_script():
     work_group_table = get_record(TableInfo, {'name': 'work_group'})
-    obj = insert_record(ActionInfo, {'action': 'copy', 'label': 'Копировать группу', 'table': work_group_table.id})
+    obj = get_or_insert(ActionInfo, {'action': 'copy', 'label': 'Копировать группу', 'table': work_group_table.id})
     return bool(obj)
